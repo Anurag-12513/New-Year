5f5c4004fc37750a2d899940a29f5627edfdd49e
Anurag-12513 <anuragshandilya1122@gmail.com>
2024-01-01 22:07:44 +0530

    Initilizing all the files

---
+++ script.js
@@ -0,0 +1,10 @@
+let b1= document.getElementById('btn');
+let b2= document.getElementById('btn2');
+
+b1.addEventListener('click', ()=>{
+    window.location.href = 'page2.html';
+})
+
+b2.addEventListener('click', ()=>{
+    window.location.href = 'page3.html';
+})
